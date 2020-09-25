3c306295ecf4255124edcda0ac53dfee33c7eead
AlexOdnov <alex.odnoralov@yandex.ru>
2020-09-25 18:57:47 +0300

    add contacts block

--- src/js/index.js
+++ src/js/index.js
@@ -11,4 +11,26 @@ const swiperFeedback = new Swiper('.slider-feedback', {
   },
 });
 
+const init = () => {
+  const myMap = new ymaps.Map('map', {
+    center: [59.93944115603922, 30.32302403991186],
+    zoom: 16,
+  });
+
+  const placemark = new ymaps.Placemark(
+    [59.938633647616214, 30.32304549758399],
+    {},
+    {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/backgrounds/map-pin.svg',
+      iconImageSize: [66, 101],
+      iconImageOffset: [-33, -101],
+    }
+  );
+
+  myMap.behaviors.disable('scrollZoom');
+  myMap.geoObjects.add(placemark);
+};
+
 mobileMenu();
+ymaps.ready(init);

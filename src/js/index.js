56038f3dd03b66f02d41a80806ad1514855003c8
AlexOdnov <alex.odnoralov@yandex.ru>
2020-09-25 15:55:03 +0300

    add feedback block

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
 import mobileMenu from './mobileMenu';
+import { Swiper, Navigation } from 'swiper';
+
+Swiper.use(Navigation);
+
+const swiperFeedback = new Swiper('.slider-feedback', {
+  loop: true,
+  navigation: {
+    nextEl: '.slider-feedback__next',
+    prevEl: '.slider-feedback__prev',
+  },
+});
 
 mobileMenu();
